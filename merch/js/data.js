e3872afab2c16dee36c69a624c046b525343fbdb
good161 <levaefremov2002@gmail.com>
2026-07-21 22:18:21 +0300

    Update data.js

--- merch/js/data.js
+++ merch/js/data.js
@@ -1,15 +1,9 @@
 function formatPrice(value) {
     if (!value) return 'По запросу';
-    return new Intl.NumberFormat('ru-RU', {
-        style: 'currency',
-        currency: 'RUB',
-        minimumFractionDigits: 0
-    }).format(value);
+    return new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB', minimumFractionDigits: 0 }).format(value);
 }
 
 function isProductAvailable(product) {
-    if (product.variants && product.variants.length > 0) {
-        return product.variants.some(v => v.inStock !== false);
-    }
+    if (product.variants && product.variants.length > 0) return product.variants.some(v => v.inStock !== false);
     return product.inStock !== false;
 }
