f11512c694503118c023be952ec33981c9e4f223
good161 <levaefremov2002@gmail.com>
2026-07-25 19:10:02 +0300

    Delete merch/js/admin-ui.js

--- merch/js/admin-ui.js
+++
@@ -1,4 +0,0 @@
-function initAdminUI() {
-    document.getElementById('scrollTopBtn').addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
-    document.getElementById('scrollBottomBtn').addEventListener('click', () => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' }));
-}
