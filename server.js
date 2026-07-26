12c4422653c60e4b80600a0d30ce766c206a21d1
good161 <levaefremov2002@gmail.com>
2026-07-26 08:53:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: (process.env.DATABASE_URL || '').replace('?sslmode=require', ''),
     ssl: { rejectUnauthorized: false }
 });
 
