b338e37f75c8f4b30b76702cdb69ea5814941af1
good161 <levaefremov2002@gmail.com>
2026-08-04 15:25:18 +0300

    Update admin.js

--- merch/js/admin.js
+++ merch/js/admin.js
@@ -15,16 +15,14 @@ function initAdmin() {
             isAdmin = false;
             adminRole = '';
             showArchived = false;
-            authToken = '';
             localStorage.removeItem('isAdmin');
-            localStorage.removeItem('authToken');
             localStorage.removeItem('adminRole');
             renderProducts(products);
         } else {
             showLoginModal();
         }
     });
-
+    
     initAdminAuth();
     initAdminProducts();
     initAdminManagers();
