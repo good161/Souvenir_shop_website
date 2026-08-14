7544415cf6fe293c7530bcfc1c5c22d61398e284
Danii1Anisimov <megaputino@gmail.com>
2026-08-14 15:30:18 +0300

    Update admin.js

--- merch/js/admin.js
+++ merch/js/admin.js
@@ -1,10 +1,19 @@
+let isAdmin = false;
+let adminRole = '';
 let editingProductId = null;
 let showArchived = false;
 
+// Просто проверяем, есть ли админ в localStorage
+if (localStorage.getItem('isAdmin') === 'true') {
+    isAdmin = true;
+    adminRole = localStorage.getItem('adminRole') || '';
+}
+
 function initAdmin() {
-    
-    renderProducts(products);
+    // Только инициализация продуктов
     initAdminProducts();
 }
+
+
 
 initAdmin();
