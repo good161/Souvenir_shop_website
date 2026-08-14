beaa0ebcf0d47295e13c59e59d0f7d336e9907ec
Danii1Anisimov <megaputino@gmail.com>
2026-08-14 15:11:49 +0300

    Update admin.js

--- merch/js/admin.js
+++ merch/js/admin.js
@@ -1,33 +1,8 @@
-let isAdmin = false;
-let adminRole = '';
 let editingProductId = null;
 let showArchived = false;
 
-if (localStorage.getItem('isAdmin') !== 'true') {
-    isAdmin = false;
-    adminRole = '';
-}
-
 function initAdmin() {
-    document.getElementById('adminBtn').addEventListener('click', () => {
-        if (isAdmin) {
-            document.getElementById('adminBtn').classList.remove('active');
-            isAdmin = false;
-            adminRole = '';
-            showArchived = false;
-            authToken = '';
-            localStorage.removeItem('isAdmin');
-            localStorage.removeItem('authToken');
-            localStorage.removeItem('adminRole');
-            renderProducts(products);
-        } else {
-            showLoginModal();
-        }
-    });
-    
-    initAdminAuth();
-    initAdminProducts();
-    initAdminManagers();
+    renderProducts(products);
 }
 
 initAdmin();
