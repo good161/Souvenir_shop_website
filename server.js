8da8369400c00164c4182866d511fe9d3c5e57c8
good161 <levaefremov2002@gmail.com>
2026-09-21 00:50:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname)));
 const JWT_SECRET = process.env.JWT_SECRET || 'chsu-merch-jwt-secret-2026';
 
 const pool = new Pool({
-    connectionString: 'postgres://postgres.atzspalpmoijomeccjzw:ujuoiYHU784E87H-0IknuojDUO@aws-0-us-east-1.pooler.supabase.com:6543/postgres',
+    connectionString: 'postgresql://postgres.tomujzfwckslaylivjiq:ujuoiYHU784E87H-0IknuojDUO@aws-0-us-east-1.pooler.supabase.com:6543/postgres',
     ssl: { rejectUnauthorized: false }
 });
 
